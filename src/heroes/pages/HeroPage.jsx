c44de41f901e6d1c436ed077c12064c363dee027
Lagarde <jose.a.lagarde@accenture.com>
2023-06-21 09:39:52 -0600

    first commit

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,53 @@
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+import { useMemo } from 'react';
+
+export const HeroPage = () => {
+
+    const { heroId } = useParams();
+    const navigate = useNavigate();
+
+    const hero = useMemo( 
+        () => getHeroById(heroId), 
+        [heroId]
+    ); 
+
+    const onNavigateBack = () => {
+        navigate(-1);
+    }
+
+    if (!hero) {
+        return <Navigate to="/marvel" />
+    }
+
+    return (
+        <div className='row mt-5'>
+            <div className="col-4">
+                <img
+                    src={`/assets/heroes/${heroId}.jpg`}
+                    alt={hero.superhero}
+                    className='img-thumbnail animate__animated animate__fadeInLeft'
+                />
+            </div>
+
+            <div className="col-8">
+                <h3>{hero.superhero}</h3>
+                <ul className='"list-group list-group-flush'>
+                    <li className='list-group-item'> <b>Alter ego: {hero.alter_ego}</b> </li>
+                    <li className='list-group-item'> <b>Publisher: {hero.publisher}</b> </li>
+                    <li className='list-group-item'> <b>First Appearance: {hero.first_appearance}</b> </li>
+                </ul>
+
+                <h5 className='mt-3'>Characters</h5>
+                <p> {hero.characters} </p>
+
+                <button
+                    className='btn btn-outline-primary'
+                    onClick={onNavigateBack}
+                >
+                    Regresar
+                </button>
+            </div>
+        </div>
+    )
+}
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,14 @@
+import { HeroesList } from "../components"
+
+export const MarvelPage = () => {
+    return (
+        <>
+            <h1>Mavel Comics</h1>
+
+            <hr />
+
+            <HeroesList publisher="Marvel Comics"/>
+        </>
+        
+    )
+}
